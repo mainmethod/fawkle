3fd06ce5c537f45f0ceaf616dfc5c00903e16705
Rob Farrell <codeninja@local.local>
2013-10-04 21:47:10 -0400

    inital app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fawkle::Application.config.secret_token = '0afa546b7196f0472221eea3bc3dd1b4e8da143ba00fe445d0d5da6955e1c147e5c3b8d89bbea928c3fca5c7433075543ced3c9998941a92a1dafd827a6f3fe2'
