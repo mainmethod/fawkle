2d6b36b36d75ab4707bf5ef2dd054f06dde416a5
Ben Walker <bwalker@asheavenue.com>
2014-07-24 11:24:28 -0400

    some stuff?

---
+++ app/assets/javascripts/fawkle.js
@@ -0,0 +1,21 @@
+var fawkle = (function(){
+
+    var self;
+
+    return {
+
+        init: function(){
+
+            self = this;
+        },
+
+        selectDie: function(){
+            
+        }
+
+    };
+
+})();
+$(document).ready(function(){
+    fawkle.init();
+});
